a9e1bbe995efe520d6ac3bea03df5fe83bc653ca
jarvisar <jarvisar@mail.uc.edu>
2023-03-18 19:19:59 -0400

    Tried to fix issue when loading timeline

--- js/main.js
+++ js/main.js
@@ -289,7 +289,7 @@ d3.csv('data/311_data_pt_2.csv')
 
     loading.classList.remove("loading"); // Remove loading message
 
-  }, 100) // Use setTimeout to delay the loading message (prevent null classlist error)
+  }, 120) // Use setTimeout to delay the loading message (prevent null classlist error)
 })
 .catch(error => console.error(error));
 
